bf85c43a635c03a6092c1251007010a47fa775b4
DiodeCN <1069523953@qq.com>
2023-06-28 12:34:06 +0800

    11

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+import cv2
+
+# 创建一个VideoCapture对象，参数0表示使用默认的摄像头
+cap = cv2.VideoCapture(0)
+
+while True:
+    # 从摄像头读取帧
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # 显示帧
+    cv2.imshow('Camera', frame)
+
+    # 按下q键时，退出循环
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# 释放摄像头资源并关闭窗口
+cap.release()
+cv2.destroyAllWindows()
