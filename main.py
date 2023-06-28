e9e53d93a05ae3761b26f9e073a8cc6e46f2b414
DiodeCN <1069523953@qq.com>
2023-06-28 14:13:04 +0800

    11

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import tkinter as tk
-import PIL.Image, PIL.ImageTk
+from PIL import Image, ImageTk
 
 # 创建一个VideoCapture对象，参数0表示使用默认的摄像头
 cap = cv2.VideoCapture(0)
@@ -25,20 +25,27 @@ while True:
     if not ret:
         break
 
+    # 更新窗口尺寸
+    window_width = root.winfo_width()
+    window_height = root.winfo_height()
+    if window_width / window_height > aspect_ratio:
+        window_width = window_height * aspect_ratio
+    else:
+        window_height = window_width / aspect_ratio
+
+    root.geometry("%dx%d" % (int(window_width), int(window_height)))
+
     # 将OpenCV图像转换为Tkinter图像
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+    frame = Image.fromarray(frame)
+    frame = frame.resize((int(window_width), int(window_height)), Image.ANTIALIAS)
+    frame = ImageTk.PhotoImage(image=frame)
 
     # 在Canvas上绘制图像
     canvas.create_image(0, 0, anchor = tk.NW, image = frame)
 
-    # 更新窗口尺寸
-    window_width = root.winfo_width()
-    window_height = root.winfo_height()
-    if window_width / window_height != aspect_ratio:
-        # 保持宽高比不变
-        window_height = window_width / aspect_ratio
-        root.geometry("%dx%d" % (window_width, int(window_height)))
+    # 更新Canvas的尺寸
+    canvas.config(width=int(window_width), height=int(window_height))
 
     # 更新Tkinter窗口
     root.update()
